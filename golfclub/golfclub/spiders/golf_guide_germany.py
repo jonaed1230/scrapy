e60a65ba01a21ed540fd012ac3b5f83714cda29b
Md Jonaed Hasan <jonaed.com.bd@gmail.com>
2018-11-30 06:20:57 +0600

    Update golf_guide_germany.py

--- golfclub/golfclub/spiders/golf_guide_germany.py
+++ golfclub/golfclub/spiders/golf_guide_germany.py
@@ -11,20 +11,22 @@ class GolfGuideGermanySpider(scrapy.Spider):
         listings = response.xpath('//h4[@class="item-box__title"]')
 
         for listing in listings:
+            name = listing.xpath('.//a[@class="item-box__title-text"]/text()').extract_first()
             link = listing.xpath('.//a[@class="item-box__title-text"]/@href').extract_first()
             absolute_link = response.urljoin(link)
-            yield scrapy.Request(absolute_link, callback=self.parse_listing)
+            yield scrapy.Request(absolute_link, callback=self.parse_listing, 
+                                meta={'name': name})
 
         next_page_url = response.xpath('//a[@class="link-button pagination__button pagination__button--next link-button--small link-button--light"]/@href').extract_first()
         if next_page_url:
             yield scrapy.Request(response.urljoin(next_page_url), callback=self.parse)
 
     def parse_listing(self, response):
-        club_name = response.xpath('//div[@class="row__content"]/h1[@class="row__title row__title--medium"]/text()').extract_first()
+        club_name = response.meta['name']
         club_address = response.xpath('//p[@class="address-view__addr adr"]/text()').extract_first()
         club_number = response.xpath('//span[@class="tel"]/text()').extract_first()
         club_email = response.xpath('//a[@class="email"]/text()').extract_first()
         yield {'Club name': club_name,
                'Club Address': club_address,
                'Club Number': club_number,
-               'Club E-mail': club_email}         
+               'Club E-mail': club_email}                
